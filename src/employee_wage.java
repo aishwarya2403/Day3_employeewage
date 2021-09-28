855e1233b8f4f6197d4dfda67203ff9499c76c44
gaikwad.aishwarya06@gmail.com <email-id>
2021-09-28 19:15:15 +0530

    EmployeeWage

---
+++ src/employee_wage.java
@@ -0,0 +1,39 @@
+import java.util.Random;
+public class employee_wage{
+        public static void main(String[] args) {
+            int rate=20, empHrs=0, totalSalary=0, count=0;
+            Random days = new Random();
+            int number;
+            for (int i=1; i<=20; i++) {
+                number = days.nextInt(3);
+                System.out.println(number + " ");
+                if (empHrs<=100) {
+                    switch (number) {
+                        case 1:
+                            empHrs= empHrs+8;
+                            break;
+                        case 2:
+                            empHrs=empHrs+4;
+                            break;
+                        default:
+                            empHrs=empHrs+0;
+                    }
+                    count = count+1;
+                    System.out.println(count);
+                    System.out.println(empHrs);
+                }
+                //System.out.println(empHrs);
+                //z= z+1;
+                //System.out.println(z);
+            }
+            int salary= empHrs*rate;
+            totalSalary= salary+totalSalary;
+            System.out.println("\nTotal salary is "+ totalSalary);
+        }
+    }
+
+
+
+
+
+
